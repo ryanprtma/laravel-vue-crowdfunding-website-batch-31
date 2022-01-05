9f9de55846e90e92b6fa7b3daca31e758e8439d5
ryanprtma <ryanapratama16@gmail.com>
2022-01-05 07:26:24 +0700

    update tugas 2

---
+++ crowdfunding-website/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            // $table->bigIncrements('id');
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->string('name');
+            $table->uuid('role_id')->unsigned();
+            $table->foreign('role_id')->references('id')->on('roles');
+            // $table->timestamp('email_verified_at')->nullable();
+            // $table->string('password');
+            // $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
