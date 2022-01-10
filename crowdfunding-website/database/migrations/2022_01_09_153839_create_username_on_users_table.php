d932a99734c7fe67514aba0162096788ad8a8cfb
ryanprtma <ryanapratama16@gmail.com>
2022-01-10 07:39:20 +0700

    update tugas 4

--- crowdfunding-website/database/migrations/2022_01_09_153839_create_username_on_users_table.php
+++ crowdfunding-website/database/migrations/2022_01_09_153839_create_username_on_users_table.php
@@ -14,7 +14,7 @@ class CreateUsernameOnUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('username')->after('name');
+            $table->string('username')->unique();
         });
     }
 
